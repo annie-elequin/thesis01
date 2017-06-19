2162ed06ee05d1b3ef49f3c94357e8edf842db5c
Annie Mathis <annie_mathis@baylor.edu>
2017-06-19 11:24:47 -0500

    dynamic table working. not square yet though.

--- thesis.js
+++ thesis.js
@@ -12,38 +12,46 @@ if(Meteor.isClient){
         'selectedClass': function(){
             var selectedClass = Session.get('selectedClass');
             console.log("we are in selectedClass, so "+selectedClass);
-            // return ClassList.findOne({ _id: selectedClass._id });
+            return ClassList.findOne({ room: selectedClass });
         },
         'createTable': function(){
             console.log("createTable called");
-            // var cur = Session.get('selectedClass');
-            /*
-            var cur = Session.get('selectedClass');
-            var code = "<TABLE>";
-            for(i=0;i<cur.rows;i++){
-                code += "<TR>";
-                for(j=0;j<cur.cols+cur.aisles;j++){
-                    code += "<TD></TD>";
+            var temp = Session.get('selectedClass');
+            var cur = ClassList.findOne({ room: temp });
+            // var code = "";
+            // var code = "<TABLE>";
+            var table = document.getElementById('myTable');
+            if(table){
+                table.innerHTML = "";
+                for(i=0;i<cur.rows;i++){
+                    // code += "<TR>";
+                    var newRow = table.insertRow(i);
+                    console.log("insertrow called");
+                    for(j=0;j<cur.cols+cur.aisles;j++){
+                        // code += "<TD></TD>";
+                        var newCell = newRow.insertCell(j);
+                        console.log("insertcell called");
+                        var newText = document.createTextNode('New Row');
+                        newCell.appendChild(newText);
+                    }
+                    // code += "</TR>";
                 }
-                code += "</TR>";
             }
-            code += "</TABLE>";
-            console.log(code);
-            */
+            else{
+                console.log("There was no myTable");
+            }
+            // code += "</TABLE>";
+            // console.log(code);
+            // document.getElementById("mytable").innerHTML = code;
             return "wat";
         }
     });
 
     Template.classroomlayout.events({
         'change .classroomdropdown': function(){
-            console.log("chosen: "+event.target.value);
-            var cur = ClassList.find({ room: event.target.value });
-            var selectedClassroom = cur._id;
-            Session.set('selectedClass', );
+            // console.log("chosen: "+event.target.value);
+            Session.set('selectedClass', event.target.value);
             console.log("change detected.");
-            console.log(selectedClassroom);
-            console.log(selectedClassroom._id);
-            console.log(selectedClassroom.room);
         }
     });
 }
