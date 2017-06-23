feb79a5b0d671d3335977738b585ca672cfec412
Annie Mathis <annie_mathis@baylor.edu>
2017-06-23 12:11:19 -0500

    trying to add status "active" to a logged in user

--- thesis.js
+++ thesis.js
@@ -1,20 +1,40 @@
 ClassList = new Mongo.Collection('classrooms');
 CourseList = new Mongo.Collection('courses');
+UserList = new Mongo.Collection('myusers');
+SeatList = new Mongo.Collection('seats');
 
 if(Meteor.isClient){
     Meteor.subscribe('theClasses');
 
     // HELPER functions for the CLASSROOM LAYOUT
-    Template.classroomlayout.helpers({
+    Template.chooseclassroom.helpers({
         'class': function(){
             // return ClassList.find();   
             return ClassList.find();        
+        }
+    });
+
+    // HELPER functions for the CLASSROOM LAYOUT
+    Template.classroomlayout.helpers({
+        'setActive': function(){
+            console.log("setActive");
+            var curUser = Meteor.userId();
+            // UserList.update({ ID: curUser},
+            //                 { $set: 
+            //                     { status: "active" }});
+            var myUser = UserList.findOne({ ID: curUser });
+            // myUser.status = "active";
+            console.log(myUser);
+            // console.log(thisrole);
         },
         'selectedClass': function(){
             var selectedClass = Session.get('selectedClass');
             console.log("we are in selectedClass, so "+selectedClass);
             return ClassList.findOne({ room: selectedClass });
         },
+        // 'isStudent': function(){
+        //     var 
+        // },
         'createTable': function(){
             console.log("createTable called");
             var temp = Session.get('selectedClass');
@@ -32,8 +52,8 @@ if(Meteor.isClient){
                         // newCell.appendChild(newText);
                         var numSeats = (cur.cols / (cur.aisles+1))+1;
                         if((j+1)%numSeats == 0){
-                            console.log(j+1);
-                            console.log("cols/aisles what="+numSeats);
+                            // console.log(j+1);
+                            // console.log("cols/aisles what="+numSeats);
                             newCell.innerHTML = "<div class='aisle'>aisle</div>";
                             newCell.style.background = "black";
                             newCell.style.width = "18px";
@@ -49,11 +69,16 @@ if(Meteor.isClient){
         }
     });
 
-    Template.classroomlayout.events({
-        'change .classroomdropdown': function(){
+    Template.chooseclassroom.events({
+        'change .classroomdropdown': function(event){
             // console.log("chosen: "+event.target.value);
             Session.set('selectedClass', event.target.value);
             console.log("change detected.");
+            Meteor.userId();
         }
+    });
+
+    Template.classroomlayout.events({
+
     });
 }
