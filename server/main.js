67ee7e6d967567d38badf6fb5b457288403db1db
Annie Mathis <annie_mathis@baylor.edu>
2017-08-17 13:55:26 -0500

    minor updates

--- server/main.js
+++ server/main.js
@@ -1,9 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { SeatList } from '/imports/api/SeatList';
 import { Questions } from '/imports/api/Questions';
-
-// SeatList = new Mongo.Collection('seatlist');
-// Questions = new Mongo.Collection('questions');
+import { Log } from '/imports/api/Log';
 
 Meteor.startup(() => {
   // code to run on server at startup
